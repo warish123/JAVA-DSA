eeb38fd93afef502f5b6ccd9f330dcf45a8fd226
warish123 <133870135+warish123@users.noreply.github.com>
2024-11-10 20:28:00 +0530

    added test.java

---
+++ Arrays/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,4};
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
